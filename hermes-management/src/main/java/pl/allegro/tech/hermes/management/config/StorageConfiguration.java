56380b38787b31dced3cc52489fba8d60b1b57e6
wojtkiewicz <wojtkiewicz.bartosz@gmail.com>
2016-11-29 12:13:07 +0100

    630 fixed bug with retransmission node being removed before consumption

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/config/StorageConfiguration.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/config/StorageConfiguration.java
@@ -144,11 +144,11 @@ public class StorageConfiguration {
     }
 
     @PostConstruct
-    public void ensureInitPathExists(AdminTool adminTool) throws Exception {
+    public void ensureInitPathExists() throws Exception {
         if (storageZookeeper().checkExists().forPath(zookeeperPaths().groupsPath()) == null) {
             storageZookeeper().create().creatingParentsIfNeeded().forPath(zookeeperPaths().groupsPath());
         }
-        adminTool.start();
+        adminTool().start();
     }
 
 }
